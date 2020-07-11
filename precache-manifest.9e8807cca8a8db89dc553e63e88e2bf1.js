4d32488ead8e79b395873b8f73407fd43c4beb59
EsraaMamoun <esraamamoun5@gmail.com>
2020-07-11 22:00:46 +0300

    Updates

---
+++ precache-manifest.9e8807cca8a8db89dc553e63e88e2bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c4b6d2324220e172cdf9b9cf5d6de7",
+    "url": "/apiTodo/index.html"
+  },
+  {
+    "revision": "40f4e1380646f67d3370",
+    "url": "/apiTodo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "386e68f6e7e614ed2c29",
+    "url": "/apiTodo/static/css/main.9c447df1.chunk.css"
+  },
+  {
+    "revision": "40f4e1380646f67d3370",
+    "url": "/apiTodo/static/js/2.fcbfdae7.chunk.js"
+  },
+  {
+    "revision": "d3286c608b7b6d68fb79cf25ce73f2fb",
+    "url": "/apiTodo/static/js/2.fcbfdae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386e68f6e7e614ed2c29",
+    "url": "/apiTodo/static/js/main.ec246738.chunk.js"
+  },
+  {
+    "revision": "69e31367b6b3582e2ffa",
+    "url": "/apiTodo/static/js/runtime-main.1daf1f0d.js"
+  }
+]);
