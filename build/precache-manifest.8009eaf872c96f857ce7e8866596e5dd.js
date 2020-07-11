a3b427c0748c38ebe7298394a35957b74f780b8d
EsraaMamoun <esraamamoun5@gmail.com>
2020-07-11 22:06:54 +0300

    gh-pages

---
+++ build/precache-manifest.8009eaf872c96f857ce7e8866596e5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2df4569f85269e1c45fee9494eef262",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "9416cc6ba9a04f91b81c",
+    "url": "/todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ea87b3a31cfb3d6c8f1",
+    "url": "/todo/static/css/main.9c447df1.chunk.css"
+  },
+  {
+    "revision": "9416cc6ba9a04f91b81c",
+    "url": "/todo/static/js/2.5bba4e85.chunk.js"
+  },
+  {
+    "revision": "d3286c608b7b6d68fb79cf25ce73f2fb",
+    "url": "/todo/static/js/2.5bba4e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea87b3a31cfb3d6c8f1",
+    "url": "/todo/static/js/main.b1c9a74e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
